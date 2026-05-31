f7a6d2c3cc4561bec970cd4c5af3ff84ac011b55
dieudonneWillems <don_willems@me.com>
2026-05-31 14:47:43 +0200

    Initial Commit

---
+++ Navi/NaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviApp.swift
+//  Navi
+//
+//  Created by Dieudonné Willems on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct NaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NaviTests/NaviTests.swift
@@ -0,0 +1,17 @@
+//
+//  NaviTests.swift
+//  NaviTests
+//
+//  Created by Dieudonné Willems on 31/05/2026.
+//
+
+import Testing
+@testable import Navi
+
+struct NaviTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
